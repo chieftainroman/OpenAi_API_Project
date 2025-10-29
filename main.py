00cbd49391fab91dd4bffb94f719e573a72d50b5
Roman <romanmammadov872@gmail.com>
2025-10-29 14:29:14 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os 
+
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+genre =  input("Enter a genre for the story: ")
+
+user_input = ""
+
+while user_input != "exit":
+    
+    user_input = input("Ask something: ")
+    
+    if user_input != "exit":
+        response = client.chat.completions.create(
+            model = "gpt-3.5-turbo",
+            max_tokens=200,
+            messages = [
+                {"role": "system", "content": "You are a creative storyteller"},
+                {"role": "user", "content": "Write a short story about" + user_input + " in the genre of " + genre},
+            ]
+        )
+        
+        print(response.choices[0].message.content)
+
+
